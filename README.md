0e9e0a0af3860c36eb31f2ee93c05d2d337afa29
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-01-10 16:34:51 -0800

    is this working

---
+++ README.md
@@ -0,0 +1,9 @@
+# LAB - Class 17
+
+## Project: Web Scraper
+
+## Author: Cody De La Torre
+
+### Resources
+
+[Beautiful soup docs](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
--- scraper.py
+++ scraper.py
@@ -1,8 +1,34 @@
 import requests
-from parser import parse
+from bs4 import BeautifulSoup
+import re
+
+url = 'https://en.wikipedia.org/wiki/Spider-Man'
+
+
+def helper(r):
+    soup = BeautifulSoup(r.text, "html.parser")
+    return soup.find_all(title="Wikipedia:Citation needed")
+
+
+def polish(text):
+    polished = re.sub(r"\[(.*?)\]", "", text)
+    return polished
+
+def get_citations_needed_count(url):
+  needs = helper(requests.get(url))
+  print("Number of citations needed: ", len(needs))
+  
+
+def get_citations_needed_report(url):
+  try:
+    needs = helper(requests.get(url))
+    for need in needs:
+      text = polish(need.find_parent("p").text)
+      print(text)
+  except:
+      print("No citations needed")
+
 
 if __name__ == '__main__':
-    url = 'https://testing-www.codefellows.org/course-calendar/?filters=400:%20Advanced,code-python-401'
-    response = requests.get(url)
-    results = parse(response.text)
-    print(results)
+     get_citations_needed_count(url)
+     get_citations_needed_report(url)
